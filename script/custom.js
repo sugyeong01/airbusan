a95d89720c37426a4c35e62d2894d29762e29e2a
sugyeong01 <2sugyeong01@gmail.com>
2025-05-12 19:53:03 +0900

    mac20250512

--- script/custom.js
+++ script/custom.js
@@ -2,8 +2,16 @@
 $(function(){
     $(".ham").on("click", function(){
         $(".menu").css("display", "block");
+        $("body").css({
+            height: "100vh",
+            overflow: "hidden"
+        });
     });
     $(".xmark").on("click", function(){
         $(".menu").css("display", "none");
+        $("body").css({
+            height: "auto",
+            overflow: "auto"
+        });
     });
 });
