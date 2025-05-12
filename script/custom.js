aa77a7a8bafe8ee7ddf1df5a1075074837c72bb3
sugyeong01 <2sugyeong01@gmail.com>
2025-05-13 01:03:08 +0900

    mac20250512-2

--- script/custom.js
+++ script/custom.js
@@ -1,17 +1,17 @@
 // hidemenu
-$(function(){
-    $(".ham").on("click", function(){
-        $(".menu").css("display", "block");
-        $("body").css({
-            height: "100vh",
-            overflow: "hidden"
-        });
+ $(function(){
+     $(".ham").on("click", function(){
+         $(".menu").css("display", "block");
+         $("body").css({
+             height: "100vh",
+             overflow: "hidden"
+         });
+     })
+     $(".xmark").on("click", function(){
+         $(".menu").css("display", "none");
+         $("body").css({
+             height: "auto",
+             overflow: "auto"
+         });
+     });
     });
-    $(".xmark").on("click", function(){
-        $(".menu").css("display", "none");
-        $("body").css({
-            height: "auto",
-            overflow: "auto"
-        });
-    });
-});
